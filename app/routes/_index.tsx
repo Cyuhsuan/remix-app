3c600af2f2e23fc563b617b8985ac0b8eb6ac0c2
yu <x89301089@gmail.com>
2024-11-06 11:11:31 +0800

    init

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+export default function Index() {
+    return (
+      <p id="index-page">
+        This is a demo for Remix.
+        <br />
+        Check out{" "}
+        <a href="https://remix.dev.org.tw">the docs at remix.run</a>.
+      </p>
+    );
+  }
+  
